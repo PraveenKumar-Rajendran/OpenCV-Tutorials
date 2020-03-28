7f3daffc5b1b6d7b48b08b9e3387b84de11b78e1
Praveenkumar1996 <aravindhanpraveen19@gmail.com>
2020-03-28 12:54:43 +0530

    Getting Started with Images Tutorial of OpenCV is implemented. Comments are writen to understand the flow. Required data also updated in the current working directory.

--- 01_20200328/01_Getting_Started_with_Images.py
+++ 01_20200328/01_Getting_Started_with_Images.py
@@ -1,1 +1,30 @@
-print("Hello world")
+import numpy as np
+import cv2
+
+# Load an color image in grayscale
+img1 = cv2.imread(r'Audi.jpg',1)
+img2 = cv2.imread(r'Audi.jpg',0)
+img3 = cv2.imread(r'Audi.jpg',-1)
+img4 = cv2.imread(r'Ronaldo.jpg',-1)
+
+
+#print(img)   #printing out the array representation for the image, If the image is not loaded it returns None.
+
+#Displaying the Colour version of the image (Alpha channel is ignored)
+cv2.imshow('image_Colour',img1)
+#Displaying the grayscale version of the image
+cv2.imshow('image_Grayscale',img2)
+#Displaying the unchanged version of the image
+cv2.imshow('image_Unchanged',img3)
+#Here comes Ronaldo, to finish it off
+cv2.imshow('Ronaldo',img4)
+
+
+#cv2.waitKey(0)     #given parameter specifies the milliseconds for the wait time, for 0 it waits until any keystroke
+k = cv2.waitKey(0)  #waitKey() Returns the ascii value of the pressed key and waits for the specified time
+print(k)
+cv2.destroyAllWindows()  # closes all the windows
+
+#To save back the processed images
+
+cv2.imwrite('AudiGrey.png',img2)
